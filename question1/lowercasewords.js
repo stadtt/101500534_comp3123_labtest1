78790f44f35d6bee048eab7c5357abcab94480d2
Shaffaq Hai <hai.shaffaq@gmail.com>
2025-10-02 17:12:50 -0400

    finished q1

--- question1/lowercasewords.js
+++ question1/lowercasewords.js
@@ -1,5 +1,29 @@
 
-
 function lowercaseWords(array){
-    
+ let myPromise = new Promise((resolve,reject) => { 
+
+    filteredArray = array.filter((item) =>typeof item  === "string" ).map(item => item.toLowerCase());
+
+        if(filteredArray.length > 0 ){
+           resolve(filteredArray);
+        }
+        else{
+            reject("Array has no string to be filtered!");
+        }
+ } 
+    );
+    return myPromise;
+};
+
+async function callLowerCase(array){
+    try{
+        
+        let response = await lowercaseWords(array);
+        console.log(response);
+    }
+    catch (error) {
+        console.log(error);
+ } 
 }
+callLowerCase(["PIZZA",10,true,25,false,"Wings"]);
+callLowerCase([534,23423]);
